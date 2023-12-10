660bf647c5b62d56375326ed7abf1d717787ebae
Narmin-G-M <narmin.g.mammadli@gmail.com>
2023-12-10 09:41:57 +0400

    Displaying Credit Score

---
+++ src/components/CriticScrore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from '@chakra-ui/react'
+
+
+interface Props {
+    score: number
+}
+
+const CriticScrore = ({score}: Props) => {
+
+  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';  
+
+  return (
+    <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='3px'>
+        {score}
+    </Badge>
+  )
+}
+
+export default CriticScrore
